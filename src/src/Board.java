d47ccd87029298a5c2843ba8b5392458405b09b5
sushpicious <2011658@apps.nsd.org>
2019-11-12 18:18:08 -0800

    Broke up functionality into two classes. Need to implement winning.

--- src/src/Board.java
+++ src/src/Board.java
@@ -3,7 +3,7 @@ import java.awt.*;
 class Board {
 
     //Creates a array that stores if the point on the grid is occupied or not
-    private int[][] occupyStatus = new int[3][3];
+    private int[][] boardStatus = new int[3][3];
     //Creates a panel that is 600 pixels by 600 pixels
     private DrawingPanel panel = new DrawingPanel(600, 600);
     //Creates the graphics object
@@ -16,40 +16,20 @@ class Board {
      */
     Board() {
         drawLines();
-        panel.onClick(this::onClick);
-
     }
 
-    /**
-     * Called whenever the user clicks on the panel.
-     * @param x X value of user click
-     * @param y Y value of user click
-     */
-    private void onClick(int x, int y) {
-        decidePlace(x, y);
-    }
-
-    /**
-     * Decides where to place the X or the O.
-     * @param x X value of user click
-     * @param y Y value of user click
-     */
-    private void decidePlace(int x, int y) {
-        System.out.println(x + ", " + y);
-        //Checks if the box is empty before placing. If it is not empty, it is not placed.
-        if (occupyStatus[x / 200][y / 200] == 0) {
-            drawObject((x/200)*200, ((y/200) * 200) + 200, turnNumber);
-            updateArray(x, y, turnNumber);
+    void updateArray(int x, int y) {
+        System.out.println("In updateArray");
+        if(boardStatus[x/200][y/200] == 0) {
+            System.out.println("In if 0");
+            if (turnNumber % 2 == 0) {
+                boardStatus[x / 200][y / 200] = 2;
+                drawObject((x/200)*200, ((y/200) * 200) + 200);
+            } else {
+                boardStatus[x / 200][y / 200] = 1;
+            }
+            drawObject((x/200)*200, ((y/200) * 200) + 200);
             turnNumber++;
-        }
-
-    }
-
-    private void updateArray(int x, int y, int turnNumber) {
-        if(turnNumber % 2 == 0) {
-            occupyStatus[x/200][y/200] = 2;
-        } else {
-            occupyStatus[x/200][y/200] = 1;
         }
     }
 
@@ -57,15 +37,14 @@ class Board {
      *
      * @param x X value of where to draw object
      * @param y Y value of where to draw object
-     * @param turnNumber Integer representing X or O's turn
      */
-    private void drawObject(int x, int y, int turnNumber) {
+     private void drawObject(int x, int y) {
         g.setFont(new Font("Monospace", Font.PLAIN, 200));
-        if (turnNumber % 2 == 0) {
-            g.drawString("X", x + 25, y - 10);
-        } else {
-            g.drawString("O", x + 25, y - 10);
-        }
+            if (turnNumber % 2 == 0) {
+                g.drawString("X", x + 25, y - 10);
+            } else {
+                g.drawString("O", x + 25, y - 10);
+            }
     }
 
     /**
@@ -78,8 +57,8 @@ class Board {
         g.drawLine(0, 400, 600, 400);
     }
 
-    public int[][] getBoardStatus() {
-        return this.occupyStatus;
+    DrawingPanel getPanel() {
+        return this.panel;
     }
 
 }
--- src/src/Logic.java
+++ src/src/Logic.java
@@ -3,11 +3,18 @@ public class Logic {
     private static Board board;
 
     public static void main(String[] args) {
-        board = new Board();
-        Logic logic = new Logic();
+        new Logic();
     }
 
-    Logic() {
+    private Logic() {
+        board = new Board();
+        board.getPanel().onClick(this::handleClick);
+    }
+
+    private void handleClick(int x, int y) {
+        System.out.println(x + ", " + y);
+        System.out.println("In handleClick");
+        board.updateArray(x, y);
     }
 
 }

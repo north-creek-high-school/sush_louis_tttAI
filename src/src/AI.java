0b216b9640c31b50be81b1f2566283235a641791
sushpicious <2011658@apps.nsd.org>
2019-11-19 09:41:51 -0800

    Created cup class.

--- src/src/AI.java
+++ src/src/AI.java
@@ -1,14 +1,6 @@
-import java.awt.*;
+
 
 class AI {
 
-    Point takeRandomTurn(int[][] status) {
-        int randomRow = (int)(Math.random()*3);
-        int randomCol = (int)(Math.random()*3);
-        while(status[randomRow][randomCol] != 0) {
-            randomRow = (int)(Math.random()*3);
-            randomCol = (int)(Math.random()*3);
-        }
-        return new Point(randomRow*200 + 5, randomCol * 200 + 5);
-    }
+
 }
